7388bad4b3e547384ecbdffcd8e36ed0be86607c
TonyLim <tony-lim@hotmail.fr>
2021-04-25 22:25:41 +0200

    cypress test tried

--- cypress/integration/cypress.js
+++ cypress/integration/cypress.js
@@ -6,4 +6,13 @@ describe('Cypress', () => {
     it('opens the app', () => {   
         cy.visit('http://localhost:3000') 
     })
+    
+    it('it has a button', () => {
+       cy.get('button').should('have.value', '3')
+    })
+
+    it('click on button start the game', () => {
+       cy.get('button').click()
+       cy.get('div').should('have.class', 'cardboard')
+    })
 })
